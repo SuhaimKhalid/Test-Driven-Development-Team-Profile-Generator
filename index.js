bb0209da8a7c61738f16e01fe8eb29f5530bceb9
Suhaim Khalid <suhaimkhalid007@gmail.com>
2024-01-02 22:53:05 +0000

    Create Manager Function

--- index.js
+++ index.js
@@ -12,4 +12,70 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const teamMembers = [];
 
+// Function to start the app
+function initApp() {
+    // Run the create manager
+    createManager();
+}
+
+// Function to promot manager data
+function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate: answer => {
+                if (answer.length < 1) {
+                    return 'Please enter at least one character.';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's ID?",
+            validate: answer => {
+                const pass = answer.match(/^[1-9]\d*$/);
+                if (pass) {
+                    return true;
+                }
+                return 'Please enter a positive number greater than zero.';
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?",
+            validate: answer => {
+                const pass = answer.match(/\S+@\S+\.\S+/);
+                if (pass) {
+                    return true;
+                }
+                return 'Please enter a valid email address.';
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: "What is the team manager's office number?",
+            validate: answer => {
+                const pass = answer.match(/^[1-9]\d*$/);
+                if (pass) {
+                    return true;
+                }
+                return 'Please enter a positive number greater than zero.';
+            }
+        } // use Promise to get promot values 
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        createTeam();
+    });
+}
+
+initApp();
